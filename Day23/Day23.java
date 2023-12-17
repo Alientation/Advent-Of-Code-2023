60f48614bdf9fc3e28f73cc84fb7f17299daca42
Bill <billma.texas@gmail.com>
2023-12-17 14:06:03 -0600

    prep

---
+++ Day23/Day23.java
@@ -0,0 +1,55 @@
+package Day23;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Base template for each day's solution
+ * Simply toggle the constants to run the correct part and files
+ */
+public class Day23 {
+    private static final String TEST_FILES_DIRECTORY = "tests";
+    private static final String INPUT_FILES_DIRECTORY = "inputs";
+
+    private static final String DAY = "23";
+    private static final boolean RUN_TEST_INPUT = true;
+    private static final boolean RUN_PART_1 = true;
+
+    private static void part1(Scanner console) {
+
+    }
+
+    private static void part2(Scanner console) {
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
+        String file = "";
+        if (RUN_TEST_INPUT) {
+            file = "Day" + DAY + "\\" + TEST_FILES_DIRECTORY + "\\day" + DAY;
+        } else {
+            file = "Day" + DAY + "\\" + INPUT_FILES_DIRECTORY + "\\day" + DAY;
+        }
+
+        if (RUN_PART_1) {
+            file += "-p1.txt";
+        } else {
+            file += "-p2.txt";
+        }
+
+        Scanner console = new Scanner(new BufferedReader(new FileReader(file)));
+
+        if (RUN_PART_1) {
+            part1(console);
+        } else {
+            part2(console);
+        }
+        console.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Program took " + (endTime - startTime) + " ms to complete");
+    }
+}
